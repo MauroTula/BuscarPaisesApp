a8f764a92f52bd2106a50069542fa21986f1b08a
Mauro Tula <maurotula@outlook.com>
2021-06-29 09:15:58 -0300

    seccion ver pais

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -9,20 +9,26 @@ import { Country } from '../interfaces/pais.interface';
 export class PaisService {
 
   private apiUrl: string = 'https://restcountries.eu/rest/v2'
- 
 
-  constructor( private http: HttpClient) { }
 
-  buscarPais(termino: string): Observable<Country[]>{
+  constructor(private http: HttpClient) { }
+
+  buscarPais(termino: string): Observable<Country[]> {
     const url = `${this.apiUrl}/name/${termino}`;
-    
+
     return this.http.get<Country[]>(url);
-    
+
   }
-  
-  buscarCapital(termino: string ): Observable<Country[]> {
+
+  buscarCapital(termino: string): Observable<Country[]> {
     const url = `${this.apiUrl}/capital/${termino}`;
 
     return this.http.get<Country[]>(url);
+  }
+
+  getPaisPorAlpha(id: string): Observable<Country> {
+    const url = `${this.apiUrl}/alpha/${id}`;
+
+    return this.http.get<Country>(url);
   }
 }
